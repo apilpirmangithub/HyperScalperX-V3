86038f5247a848b0ff9ab1180bc7a934ca761b28
apilpirmangithub <veraarina71@gmail.com>
2026-05-02 15:10:18 +0700

    chore: clean repository, only essential automaton files remained

--- automaton/scripts/extract.mjs
+++
@@ -1,7 +0,0 @@
-import fs from "fs";
-try {
-    const data = JSON.parse(fs.readFileSync("/root/automaton/optimization_report_mega.json", "utf8"));
-    console.log(JSON.stringify(data.best, null, 2));
-} catch (e) {
-    console.error(e.message);
-}
